e06101ecba8686eb25b97885b87a23896324866c
franco andree quiroga rivera <fquiroga@alumnos.inf.utfsm.cl>
2018-05-13 19:44:12 -0400

    added some structure and excuses

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # glowing-spoons
 Es un secreto. Hecho en python2 por motivos
+
--- glowing-spoons.py
+++ glowing-spoons.py
@@ -4,7 +4,7 @@ from sys import exit, argv
 def run(debug=True):
 	print "You have started:\n", 
 	print 10*'*', "Glowing Spoons", 10*'*'
-	print "by Franco Quiroga. \nWritten in python2"
+	print "by Franco Quiroga \nWritten in python2"
 	
 	if debug:
 		print "Currently, the debug option is Active"
@@ -15,21 +15,15 @@ def run(debug=True):
 
 class Engine(object):
 
-	def __init__(self, debug):
-		self.options = {}
-		self.options['debug'] = debug
+	def __init__(self, debug, tiles=False):
+		self.options = {
+		'debug': debug,
+		}
 
 		self.characters = {}
-
 		pass
 
 	def refresh(self):
 		pass
 
-class Character(object):
-
-	def __init__(self):
-		pass
-
-print "Hello world!"
 run()
