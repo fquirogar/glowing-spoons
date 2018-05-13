fd07ead2df25e56caeb55ad9fcbad867dd836400
franco andree quiroga rivera <fquiroga@alumnos.inf.utfsm.cl>
2018-05-13 06:07:57 -0400

    added engine class

--- glowing-spoons.py
+++ glowing-spoons.py
@@ -4,12 +4,27 @@ from sys import exit, argv
 def run(debug=True):
 	print "You have started:\n", 
 	print 10*'*', "Glowing Spoons", 10*'*'
-	print "A game by Franco Quiroga. \nWritten in python2"
+	print "by Franco Quiroga. \nWritten in python2"
 	
 	if debug:
 		print "Currently, the debug option is Active"
 
+	engine = Engine(debug)
+
 	return
+
+class Engine(object):
+
+	def __init__(self, debug):
+		self.options = {}
+		self.options['debug'] = debug
+
+		self.characters = {}
+
+		pass
+
+	def refresh(self):
+		pass
 
 class Character(object):
 
