9015561ce5f2e892ad88e2968ab582cb979fbb49
franco andree quiroga rivera <fquiroga@alumnos.inf.utfsm.cl>
2018-05-13 20:20:07 -0400

    added basic display function

--- glowing-spoons.py
+++ glowing-spoons.py
@@ -1,29 +1,31 @@
 from os import path
 from sys import exit, argv
 
+MAX_X = 40
+MAX_Y = 20
+
+def display_map(map, code):
+	"""
+	Takes a list (map) of lists of integers describing 
+	the 'contents' of the x and y coordinates, and displays
+	the map value mapped from the code list (integer->char)
+	"""
+	print MAX_X*"="
+	for row in map:
+		print "|"
+		for cell in row:
+			print code[cell],
+		print "|"
+	return
+
 def run(debug=True):
 	print "You have started:\n", 
-	print 10*'*', "Glowing Spoons", 10*'*'
+	print 10*'-', "Glowing Spoons", 10*'-'
 	print "by Franco Quiroga \nWritten in python2"
 	
 	if debug:
 		print "Currently, the debug option is Active"
 
-	engine = Engine(debug)
-
 	return
-
-class Engine(object):
-
-	def __init__(self, debug, tiles=False):
-		self.options = {
-		'debug': debug,
-		}
-
-		self.characters = {}
-		pass
-
-	def refresh(self):
-		pass
 
 run()
